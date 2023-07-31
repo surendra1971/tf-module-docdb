d0e47e4ed0e66b8f389545aad9e4bc230c8ed392
Your Name <you@example.com>
2023-07-31 12:41:12 +0530

    updating the Auto Push

--- docdb.tf
+++ docdb.tf
@@ -4,10 +4,20 @@ resource "aws_docdb_cluster" "docdb" {
   master_username          = "admin1"
   master_password          = "roboshop1"
   skip_final_snapshot      = true  
-  # vpc_security_group_ids   = [aws_security_group.allows_docdb.id]
-  # db_subnet_group_name     = aws_docdb_subnet_group.docdb_subnet_group.name
+  vpc_security_group_ids   = [aws_security_group.allows_docdb.id]
+  db_subnet_group_name     = aws_docdb_subnet_group.docdb_subnet_group.name
 
 
 #   backup_retention_period = 14
 #   preferred_backup_window = "02:00-05:00"
+}
+
+# creates subnet group 
+resource "aws_docdb_subnet_group" "docdb_subnet_group" {
+  name       = "roboshop-docdb-${var.ENV}-subnetgroup"
+  subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+
+  tags = {
+    Name = "roboshop-docdb-${var.ENV}-subnetgroup"
+  }
 }
