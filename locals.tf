5120ab67dff614b56bba9133f9f2f21e55812fc1
Your Name <you@example.com>
2023-08-05 13:22:42 +0530

    updating the Auto Push

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 # Whenever you have some common fields or anything which is highly used, you can consider that in locals and can call it on your need
 
 locals {
-    DOCDB_USER  = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_USERNAME"]
-    DOCDB_PASS  = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_PASSWORD"]
+    DOCDB_USERNAME  = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_USERNAME"]
+    DOCDB_PASSWORD  = jsondecode(data.aws_secretsmanager_secret_version.secret_version.secret_string)["DOCDB_PASSWORD"]
 }
